8e6b0b5ff7bbed48d7a6ed80f2e4becd96addfce
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-25 20:29:39 +0200

    añadiendo banda de gilipollas

---
+++ banda de eriad/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+//esperando a las demás cosas;
+    }
+}
---
+++ banda de eriad/src/edwin.java
@@ -0,0 +1,19 @@
+public class edwin {
+    private int edad;
+    private int huevos;
+
+    private int numuertos;
+    public edwin(){
+
+    }
+    public void setEdad(int e){
+
+        edad=e;
+    }
+
+
+
+
+
+
+}
