5da164f082b5aff13d2caaec7171d53b9b0e1f13
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-26 17:39:25 +0200

    modificacion

--- Examencod1parte2/src/Main.java
+++ Examencod1parte2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Cifra c1=new Cifra();
-        c1.dimeNum1(Integer.parseInt(JOptionPane.showInputDialog("Introduce un número: ")));
+        c1.dimeNum1(Integer.parseInt(JOptionPane.showInputDialog("Introduce un número, negro de mierda: ")));
 
 
 
