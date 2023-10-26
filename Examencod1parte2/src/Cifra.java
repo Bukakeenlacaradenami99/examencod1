5bb3029bf249519971196baa80beaa700defc587
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-26 17:37:48 +0200

    archivos cifra

---
+++ Examencod1parte2/src/Cifra.java
@@ -0,0 +1,52 @@
+public class Cifra {
+
+    private int num1;
+
+    public Cifra(){
+
+    }
+
+
+    public void dimeNum1(int n1){
+      num1=n1;
+      if(num1==0){
+          System.out.println("EL número es 0;");
+      }
+        else{
+            if(num1>0){
+                System.out.println("El número es positivo");
+            }
+            else{
+                System.out.println("El número es negativo");
+            }
+      }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Examencod1parte2/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Cifra c1=new Cifra();
+        c1.dimeNum1(Integer.parseInt(JOptionPane.showInputDialog("Introduce un número: ")));
+
+
+
+
+
+    }
+}
